0b3841cb92f4fd647ceebbf29ed51c1e3c843102
Muhammad Haikal Syahfarel <210402114@student.umri.ac.id>
2023-11-22 15:42:52 +0700

    masukkan kodingan regis ke github

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Register from './pages/register';
+// import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Register />
+    </>
+  );
+}
+
+export default App;
